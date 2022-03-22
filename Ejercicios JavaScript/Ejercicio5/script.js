832b8058f78e220d8a3209cbb3dc33c2a9d600ca
Nedd Chairi Muñoz <91211902+illoneddy@users.noreply.github.com>
2022-03-22 08:59:52 +0100

    Add files via upload

---
+++ Ejercicios JavaScript/Ejercicio5/script.js
@@ -0,0 +1,35 @@
+fuente1 = () => {
+  document.getElementById("saludo").style.fontFamily = "Times New Roman";
+};
+
+fuente2 = () => {
+  document.getElementById("saludo").style.fontFamily = "Helvetica";
+};
+
+fuente3 = () => {
+  document.getElementById("saludo").style.fontFamily = "Courier";
+};
+
+imagen1 = () => {
+  document.getElementById("imagenio").src = "/img/mbape1.jpg";
+};
+
+imagen2 = () => {
+  document.getElementById("imagenio").src = "/img/mbape2.jpg";
+};
+
+imagen3 = () => {
+  document.getElementById("imagenio").src = "/img/mbape3.jpg";
+};
+
+mensaje1 = () => {
+  document.getElementById("texto").innerHTML = "Mensaje 1";
+};
+
+mensaje2 = () => {
+  document.getElementById("texto").innerHTML = "Mensaje 2";
+};
+
+mensaje3 = () => {
+  document.getElementById("texto").innerHTML = "Mensaje 3";
+};
