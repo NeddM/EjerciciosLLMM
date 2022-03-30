6bad42a39b2a261449ccedc9f430ccf041d93cba
Nedd Chairi Muñoz <91211902+illoneddy@users.noreply.github.com>
2022-03-30 10:43:30 +0200

    Add files via upload

---
+++ Ejercicios CSS/DOM3/script.js
@@ -0,0 +1,2 @@
+const list = document.getElementById("myList");
+list.removeChild(list.firstElementChild);
---
+++ Ejercicios CSS/DOM4/script.js
@@ -0,0 +1,8 @@
+// Título del documento
+document.title
+
+// Imágenes del documento
+document.images
+
+// Enlaces del documento
+document.links
