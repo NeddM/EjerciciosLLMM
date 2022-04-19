0ac8e874fa29a9a0f5508752a4ef87af6bf019d0
Nedd Chairi Muñoz <91211902+illoneddy@users.noreply.github.com>
2022-04-19 09:08:15 +0200

    Add files via upload

---
+++ Ejercicios XML/DOM/Ejercicio2.js
@@ -0,0 +1,11 @@
+1. Muestra el título de los CDs.
+
+const lista = document.getElementsByTagName('CD').length
+const nombra = document.getElementsByTagName('CD')
+
+for(i = 0; i < lista; i++ )
+
+
+
+
+
